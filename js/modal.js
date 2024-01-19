fb41a14839beb43d851bf82493986313efc376f8
dhktjr0204 <dhktjr0204442@gmail.com>
2024-01-19 13:46:16 +0900

    [UPDATE] 이메일 유효성 검사

--- js/modal.js
+++ js/modal.js
@@ -7,11 +7,18 @@ subscribeButton.addEventListener('click',(event)=>{
     if (!email){
         alert("이메일을 입력해주세요!");
         return;
+    }else if(!isEmail(email)){
+        alert("이메일 형식에 맞게 입력해주세요");
+        return;
     }
     modal.style.display='flex';
     //모달창 뜨면 스크롤 막기
     document.body.style.overflow='hidden';
 })
+function isEmail(email){
+    let regex=/^[a-zA-Z0-9]([-_.]?[0-9a-zA-Z])*@[a-zA-Z0-9.-]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,4}$/i;
+    return regex.test(email);
+}
 
 let modalButton=document.querySelector('.modalButton');
 modalButton.addEventListener('click',()=>{
